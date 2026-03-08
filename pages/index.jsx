628eb63afefe2b5bf4976ad05d03fa5220814c37
Laura Jeffords Greenberg <35239033+JeffordsGreenberg@users.noreply.github.com>
2026-03-08 21:06:24 +0100

    Update index.jsx
    Updated line 1

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useState, useMemo, useEffect, useCallback, useRef } from "react";
 
 // ─── TOKENS ───────────────────────────────────────────────────────────────────
